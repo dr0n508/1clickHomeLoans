ef6b247aa2e950fa70cab5ade7654b59ffdee431
dr0n508 <kornienko.andriy@gmail.com>
2017-07-02 13:16:19 +0300

    add popup

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
 $(document).ready(function () {
 
+    $('.popup-link').magnificPopup({
+        type: 'inline',
 
+        overflowY: 'auto',
+
+        closeBtnInside: true,
+        preloader: false,
+
+        midClick: true,
+        removalDelay: 300,
+        mainClass: 'my-mfp-zoom-in'
+    });
 
 });
